f4b2cbc3ebe57757c29fd7d5e548a92590fbdfff
TomHuangCN <418466938@qq.com>
2018-09-14 10:58:28 +0800

    update tracker

--- src/track.js
+++ src/track.js
@@ -2,13 +2,14 @@
  * @fileOverview track g6
  * Experience improvement program
  */
+const Util = require('./util/');
 const Global = require('./global');
 const SERVER_URL = 'https://kcart.alipay.com/web/bi.do';
 const version = require('./version');
-
+const { track } = Global;
 // 延迟发送请求
 setTimeout(function() {
-  if (Global.trackable) {
+  if (track) {
     const image = new Image();
     const newObj = {
       pg: document.URL,
@@ -17,6 +18,9 @@ setTimeout(function() {
       version,
       page_type: 'syslog'
     };
+    if (Util.isPlainObject(track)) {
+      Util.mix(newObj, track);
+    }
     const d = encodeURIComponent(JSON.stringify([ newObj ]));
     image.src = `${SERVER_URL}?BIProfile=merge&d=${d}`;
   }
